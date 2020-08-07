715803160cba896c605b0dd0a9e69725703a86ae
William Cowell <williamcowell@Williams-MBP.lan>
2020-08-07 10:55:42 +0100

    Created configuration testing

---
+++ FunctionApp.Tests/ConfigurationTests.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using FunctionApp.Funcs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace FunctionApp.Tests
+{
+    public sealed class ConfigurationTests
+    {
+        public readonly static IEnumerable<object[]> _testCases = typeof(Configuration)
+                .Assembly
+                .GetTypes()
+                .Where(ContainsAzureFunction)
+                .Select(type => new [] {type})
+                .ToArray();
+
+        private static bool ContainsAzureFunction(Type type)
+            => type.GetMethods().Any(method => method.GetCustomAttributes(typeof(FunctionNameAttribute), true).Length > 0);
+
+        [Theory]
+        [MemberData(nameof(_testCases))]
+        public void Configure_ConfiguresDependenciesFor_MyHttpFunction(Type azureFunctionType)
+        {
+            var services = new ServiceCollection();
+            Configuration.Apply(services);
+
+            var provider = services.BuildServiceProvider();
+
+            var function = provider.GetService(azureFunctionType);
+            
+            function
+                .Should()
+                .NotBeNull(
+                    because: $"Cannot initialise an instance of '{azureFunctionType}'");
+        }
+    }
+}
